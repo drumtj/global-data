3c12ecffb1282992894941e35e04171e0f778535
taejin-kim <drumtj@gmail.com>
2019-06-27 18:34:49 +0900

    'deploy'

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ let globalCfg = Object.assign({}, config);
 globalCfg.output = {
   path: path.join(__dirname, "dist"),
   library: 'GlobalData',
-  libraryTarget: "self",
+  libraryTarget: "global",
   filename: "./global-data.var.js"
 }
 
